7590be34b3f275c43f699a0374ad6f623ff28f6e
Maria_melo <maria.melo.robles@gmail.com>
2024-03-04 13:50:05 -0300

    Se implementa tooltip y alert en formulario, boton Enviar

--- Apoyo prueba/assets/js/script.js
+++ Apoyo prueba/assets/js/script.js
@@ -1,5 +1,11 @@
-//ACTIVAR TOOLTIP
+//ACTIVAR TOOLTIP FORMULARIO
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 console.log(tooltipTriggerList)
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+//ALERTA ENVIAR MENSAJE EN FORMULARIO
+
+document.getElementById('mensaje').onclick = function () {
+    alert('Tu mensaje fue enviado correctamente.');
+}
