d1eb56ae78f31e3f44e4ead3d4a1858408e1bec8
Maria_melo <maria.melo.robles@gmail.com>
2024-03-04 13:26:18 -0300

    Se agrega sección footer a index.html

---
+++ Apoyo prueba/assets/js/script.js
@@ -0,0 +1,5 @@
+//ACTIVAR TOOLTIP
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+console.log(tooltipTriggerList)
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
